715e828b036ad17ed8b9d2950b74519ff70f0ab6
cloudbells <chriszmn@gmail.com>
2019-04-23 19:03:53 +0200

    Now parses all spells for mage, some manual work is required

--- spells/spiders/main.py
+++ spells/spiders/main.py
@@ -7,7 +7,9 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import time
 
+# Represents a spell.
 class Spell:
+
     def __init__(self, level, name, rank, texture, id):
         self.level = level
         self.name = name
@@ -17,7 +19,7 @@ class Spell:
 
 class MainSpider(scrapy.Spider):
     name = 'main'
-    start_urls = ['http://classic.wowhead.com/abilities/class:8/']
+    start_urls = ["http://classic.wowhead.com/abilities/class:8/"]
     spell_urls = []
     spellCollection = []
 
@@ -28,6 +30,13 @@ class MainSpider(scrapy.Spider):
         _browser_profile.set_preference("dom.webnotifications.enabled", False)
         self.driver = webdriver.Firefox(firefox_profile=_browser_profile, executable_path='geckodriver.exe', firefox_options=options)
         self.driver.get(response.url)
+        self.clickCookies()
+        self.parsePage()
+        self.parseSpells()
+        self.writeToFile()
+    
+     # Finds and clicks all cookies buttons.
+    def clickCookies(self):
         foundFirstBtn = WebDriverWait(self.driver, 10).until(
             EC.presence_of_all_elements_located(
                 (By.XPATH, "//button[contains(text(), 'Continue Using Site')]")
@@ -39,7 +48,6 @@ class MainSpider(scrapy.Spider):
                 "//button[contains(text(), 'Continue Using Site')]"
             )
             time.sleep(3) # Needed because the second button scrolls up from bottom so Selenium can't "scroll to it".
-            print("Clicking first button...")
             firstBtn.click()
             foundSecondBtn = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_all_elements_located(
@@ -48,21 +56,27 @@ class MainSpider(scrapy.Spider):
             )
             if foundSecondBtn:
                 print("Found second button, sleeping...")
-                time.sleep(3) # Needed because the second button scrolls up from bottom so Selenium can't "scroll to it".
+                time.sleep(3)
                 secondBtn = self.driver.find_element_by_xpath(
                     "//button[contains(text(), 'Got It!')]"
                 )
-                print("Clicking second button...")
                 secondBtn.click()
-                spells = self.driver.find_elements_by_xpath(
-                    "//a[@class='listview-cleartext']"
-                )
-                lastSpell = self.driver.find_element_by_xpath(
-                    "//a[@class='listview-cleartext q1']"
-                )
-                for spell in spells:
-                    self.spell_urls.append(str(spell.get_attribute("href")))
-                self.spell_urls.append(str(lastSpell.get_attribute("href")))
+
+    # Finds all spells on the current page and puts them in a table.
+    def parsePage(self):
+        spells = self.driver.find_elements_by_xpath(
+            "//a[@class='listview-cleartext'] | //a[@class='listview-cleartext q1']"
+        )
+        for spell in spells:
+            self.spell_urls.append(str(spell.get_attribute("href")))
+        self.driver.get("https://classic.wowhead.com/abilities/class:8#100+2+1")
+        spells = self.driver.find_elements_by_xpath(
+            "//a[@class='listview-cleartext'] | //a[@class='listview-cleartext q1']"
+        )
+        for spell in spells:
+            self.spell_urls.append(str(spell.get_attribute("href")))
+    
+    def parseSpells(self):
         if len(self.spell_urls) != 0:
             for spell in self.spell_urls:
                 self.driver.get(spell)
@@ -97,25 +111,29 @@ class MainSpider(scrapy.Spider):
                 rankText = rankText[-2:] if len(rankText) > 6 else rankText[-1]
                 self.spellCollection.append(Spell(levelText, nameText, rankText, textureText, id))
             self.driver.close()
-            with open("test.txt", 'a', encoding='utf8') as f:
-                print("Outputting to file...")
-                f.write("FieldGuideMageSpells = {\n")
-                index = 1
-                lastLevel = 2
-                f.write("\t[" + lastLevel + "] = {\n")
-                for spell in self.spellCollection:
-                    if spell.level > lastLevel:
-                        f.write("\t},\n") # Close level table.
-                        lastLevel = spell.level
-                        f.write("\t[" + str(lastLevel) + "] = {\n") # Open level table.
-                        index = 1
-                    f.write("\t\t[" + str(index) + "] = {\n") # Open spell table.
-                    f.write("\t\t\t[\"name\"] = \"" + spell.name + "\",\n")
-                    f.write("\t\t\t[\"rank\"] = " + spell.rank + ",\n")
-                    f.write("\t\t\t[\"cost\"] = 0,\n")
-                    f.write("\t\t\t[\"texture\"] = \"Interface/ICONS/" + spell.texture + "\",\n")
-                    f.write("\t\t\t[\"id\"] = " + spell.id + "\n")
-                    f.write("\t\t},\n") # Close spell table.
-                    index += 1
-                f.write("}") # Close root table.
-            f.close()
+
+    # Formats each spell and outputs it to a file.
+    def writeToFile(self):
+        with open("test.txt", 'a', encoding='utf8') as f:
+            print("Outputting to file...")
+            f.write("FieldGuideMageSpells = {\n")
+            index = 1
+            lastLevel = 2
+            f.write("\t[" + str(lastLevel) + "] = {\n")
+            for spell in self.spellCollection:
+                if int(spell.level) > lastLevel:
+                    f.write("\t},\n") # Close level table.
+                    lastLevel = int(spell.level)
+                    f.write("\t[" + str(lastLevel) + "] = {\n") # Open level table.
+                    index = 1
+                f.write("\t\t[" + str(index) + "] = {\n") # Open spell table.
+                f.write("\t\t\t[\"name\"] = \"" + spell.name + "\",\n")
+                f.write("\t\t\t[\"rank\"] = " + spell.rank + ",\n")
+                f.write("\t\t\t[\"cost\"] = 0,\n")
+                f.write("\t\t\t[\"texture\"] = \"Interface/ICONS/" + spell.texture + "\",\n")
+                f.write("\t\t\t[\"id\"] = " + spell.id + "\n")
+                f.write("\t\t},\n") # Close spell table.
+                index += 1
+            f.write("\t},\n") # Close last level table.
+            f.write("}") # Close root table.
+        f.close()
