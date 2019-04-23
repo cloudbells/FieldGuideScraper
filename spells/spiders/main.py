6e3b46cd524d67aee4e0c8f6592c7a9708be35b1
cloudbells <chriszmn@gmail.com>
2019-04-23 09:53:03 +0200

    Now scrapes first page, and saves to file according to LUA format

--- spells/spiders/main.py
+++ spells/spiders/main.py
@@ -4,18 +4,26 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 
-#TODO: FORMAT OUTPUT------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Spell:
+    def __init__(self, level, name, rank, texture, id):
+        self.level = level
+        self.name = name
+        self.rank = rank
+        self.texture = texture
+        self.id = id
 
 class MainSpider(scrapy.Spider):
     name = 'main'
     start_urls = ['http://classic.wowhead.com/abilities/class:8/']
     spell_urls = []
+    spellCollection = []
 
     def parse(self, response):
         options = Options()
-        options.add_argument("--headless")
+        #options.add_argument("--headless")
         _browser_profile = webdriver.FirefoxProfile()
         _browser_profile.set_preference("dom.webnotifications.enabled", False)
         self.driver = webdriver.Firefox(firefox_profile=_browser_profile, executable_path='geckodriver.exe', firefox_options=options)
@@ -26,10 +34,12 @@ class MainSpider(scrapy.Spider):
             )
         )
         if foundFirstBtn:
+            print("Found first button, sleeping...")
             firstBtn = self.driver.find_element_by_xpath(
                 "//button[contains(text(), 'Continue Using Site')]"
             )
             time.sleep(3) # Needed because the second button scrolls up from bottom so Selenium can't "scroll to it".
+            print("Clicking first button...")
             firstBtn.click()
             foundSecondBtn = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_all_elements_located(
@@ -37,10 +47,12 @@ class MainSpider(scrapy.Spider):
                 )
             )
             if foundSecondBtn:
+                print("Found second button, sleeping...")
                 time.sleep(3) # Needed because the second button scrolls up from bottom so Selenium can't "scroll to it".
                 secondBtn = self.driver.find_element_by_xpath(
                     "//button[contains(text(), 'Got It!')]"
                 )
+                print("Clicking second button...")
                 secondBtn.click()
                 spells = self.driver.find_elements_by_xpath(
                     "//a[@class='listview-cleartext']"
@@ -52,32 +64,58 @@ class MainSpider(scrapy.Spider):
                     self.spell_urls.append(str(spell.get_attribute("href")))
                 self.spell_urls.append(str(lastSpell.get_attribute("href")))
         if len(self.spell_urls) != 0:
-            with open("test.txt", 'a', encoding='utf8') as f:
-                f.write("FieldGuideMageSpells = {\n"
-                for spell in self.spell_urls:
-                    self.driver.get(spell)
-                    # Get name.
-                    nameText = self.driver.find_element_by_xpath(
-                        "//h1[@class='heading-size-1']"
-                    ).text
-                    # Get level.
-                    levelText = self.driver.find_element_by_xpath(
-                        "//div[contains(text(), 'Level:')] | //div[contains(text(), 'Requires level')]"
-                    ).text
-                    # Get rank.
+            for spell in self.spell_urls:
+                self.driver.get(spell)
+                print("Getting name of spell...")
+                # Get name.
+                nameText = self.driver.find_element_by_xpath(
+                    "//h1[@class='heading-size-1']"
+                ).text
+                print("Getting level of spell...")
+                # Get level.
+                levelText = self.driver.find_element_by_xpath(
+                    "//div[contains(text(), 'Level:')] | //div[contains(text(), 'Requires level')]"
+                ).text
+                print("Getting rank of spell...")
+                # Get rank.
+                try:
                     rankText = self.driver.find_element_by_xpath(
                         "//b[@class='q0']"
                     ).text
-                    # Get texture.
-                    textureText = self.driver.find_element_by_xpath(
-                        "//span[@class='iconsmall']/.."
-                    ).text
-                    # Get spell ID (which is part of the URL, so slice it).
-                    id = spell[34:]
-                    # Conditionally slice level text (any level < 10 should only slice -1).
-                    levelText = levelText[-2:] if " " not in levelText[-2:] else levelText[-1]
-                    # Conditionally slice rank text (if rank > 10 then we need the last two, otherwise only last 1).
-                    rankText = rankText[-2:] if len(rankText) > 6 else rankText[-1]
-                # close root table \n}
+                except NoSuchElementException:
+                    rankText = "1" # If no rank on the spell, just put a 1.
+                print("Getting texture of spell...")
+                # Get texture.
+                textureText = self.driver.find_element_by_xpath(
+                    "//span[@class='iconsmall']/.."
+                ).text
+                # Get spell ID (which is part of the URL, so slice it).
+                id = spell[34:]
+                # Any level < 10 should only slice -1.
+                levelText = levelText[-2:] if " " not in levelText[-2:] else levelText[-1]
+                # If rank > 10 then we need the last two, otherwise only last 1.
+                rankText = rankText[-2:] if len(rankText) > 6 else rankText[-1]
+                self.spellCollection.append(Spell(levelText, nameText, rankText, textureText, id))
+            self.driver.close()
+            with open("test.txt", 'a', encoding='utf8') as f:
+                print("Outputting to file...")
+                f.write("FieldGuideMageSpells = {\n")
+                index = 1
+                lastLevel = 2
+                f.write("\t[" + lastLevel + "] = {\n")
+                for spell in self.spellCollection:
+                    if spell.level > lastLevel:
+                        f.write("\t},\n") # Close level table.
+                        lastLevel = spell.level
+                        f.write("\t[" + str(lastLevel) + "] = {\n") # Open level table.
+                        index = 1
+                    f.write("\t\t[" + str(index) + "] = {\n") # Open spell table.
+                    f.write("\t\t\t[\"name\"] = \"" + spell.name + "\",\n")
+                    f.write("\t\t\t[\"rank\"] = " + spell.rank + ",\n")
+                    f.write("\t\t\t[\"cost\"] = 0,\n")
+                    f.write("\t\t\t[\"texture\"] = \"Interface/ICONS/" + spell.texture + "\",\n")
+                    f.write("\t\t\t[\"id\"] = " + spell.id + "\n")
+                    f.write("\t\t},\n") # Close spell table.
+                    index += 1
+                f.write("}") # Close root table.
             f.close()
-        self.driver.close()
